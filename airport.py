e674b546e5a28d8e367272a67810646913c3e0e3
4ISEEK <103436910+4ISEEK@users.noreply.github.com>
2023-11-23 02:12:12 +0400

    Add files via upload
    added id primary key

--- airport.py
+++ airport.py
@@ -2,7 +2,6 @@ import sqlite3
 import datetime
 from tkinter import *
 from tkinter import messagebox
-
 global db
 global sql
 global u
@@ -21,6 +20,7 @@ sql.execute(f"""CREATE TABLE IF NOT EXISTS users(
 db.commit()
 
 sql.execute(f"""CREATE TABLE IF NOT EXISTS tickets(
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
     login TEXT,
     date TEXT,
     Go TEXT
@@ -32,7 +32,6 @@ root.title('Airport')
 root.geometry('300x300')
 #root.resizable(width=False, height=False)
 root['bg'] = 'white'
-
 def next1():
     userloginsql = user_login.get()
     userphonesql = user_phone.get()
@@ -47,7 +46,6 @@ def next1():
     else:
         messagebox.showerror(title='Airport', message='this account already exists!')
         reg_window()
-
 def singin1():
     global user_login2
     user_login2 = user_login1.get()
@@ -86,7 +84,6 @@ def reg1():
     reg_window()
 def back1():
     login_window()
-
 def back2():
     menu()
 def next2():
@@ -105,10 +102,10 @@ def next2():
     else:
         pay()
 def next3():
+    plus()
     end()
 def back3():
     date1()
-
 def end():
     frame = Frame(root, bg='white')
     frame.place(relwidth=1, relheight=1)
@@ -126,32 +123,23 @@ def login_window():
     frame.place(relwidth=1, relheight=1)
 
     main_label = Label(frame, text='SING IN', font='Arial 15 bold', bg='white', fg='black')
-
     username_label = Label(frame, text='login', font='Arial 11', bg='white', fg='gray')
     user_login1 = Entry(frame, font='Arial 11', bg='white', fg='black')
-
     userpassword_label = Label(frame, text='password', font='Arial 11', bg='white', fg='gray')
     user_password1 = Entry(frame, font='Arial 11', bg='white', fg='black')
-
     singin_button = Button(frame, text='SING IN', font='Arial 11', bg='black', fg='white', command=singin1)
-
     reg_button = Button(frame, text='REG IN', font='Arial 11', bg='black', fg='white', command=reg1)
     #___________________________________
     main_label.pack()
     username_label.pack()
-
     user_login1.pack()
-
     userpassword_label.pack()
-
     user_password1.pack()
 
     singin_button.pack(padx=10, pady=8)
     reg_button.pack(padx=10, pady=8)
     frame.mainloop()
     #_______
-
-
 def reg_window():
     global user_login
     global user_phone
@@ -159,18 +147,13 @@ def reg_window():
 
     frame = Frame(root, bg='white')
     frame.place(relwidth=1, relheight=1)
-
     main_label = Label(frame, text='Creating account', font='Arial 15 bold', bg='white', fg='black')
-
     username_label = Label(frame, text='login', font='Arial 11', bg='white', fg='gray')
     user_login = Entry(frame, font='Arial 11', bg='white', fg='black')
-
     usernumber_label = Label(frame, text='number', font='Arial 11', bg='white', fg='gray')
     user_phone = Entry(frame, font='Arial 11', bg='white', fg='black')
-
     userpassword_label = Label(frame, text='password', font='Arial 11', bg='white', fg='gray')
     user_password = Entry(frame, font='Arial 11', bg='white', fg='black')
-
     next_button = Button(frame, text='Next', font='Arial 11', bg='black', fg='white', command=next1)
     back_button = Button(frame, text='Back', font='Arial 11', bg='black', fg='white', command=back1)
     #___________________________________
@@ -188,15 +171,10 @@ def menu():
     frame = Frame(root, bg='white')
     frame.place(relwidth=1, relheight=1)
     text_label = Label(frame, text='Select where to:', font='Arial 15 bold', bg='white', fg='black', )
-
     one_button = Button(frame, text='Tokyo', font='Arial 12', bg='white', fg='black', command=one)
-
     two_button = Button(frame, text='Moscow', font='Arial 12', bg='white', fg='black', command=two)
-
     three_button = Button(frame, text='London', font='Arial 12', bg='white', fg='black', command=three)
-
     four_button = Button(frame, text='Istanbul', font='Arial 12', bg='white', fg='black', command=four)
-
     back_button = Button(frame, text='Back', font='Arial 11', bg='black', fg='white', command=back1)
     #________________________________________________
     text_label.pack()
@@ -205,28 +183,22 @@ def menu():
     three_button.pack(padx=10, pady=5)
     four_button.pack(padx=10, pady=5)
     back_button.pack(padx=10, pady=10)
-
 def one():
     global to
     to = "Tokyo"
     date1()
-
 def two():
     global to
     to = "Moscow"
     date1()
-
 def three():
     global to
     to = "London"
     date1()
-
 def four():
     global to
     to = "Istanbul"
     date1()
-
-
 def date1():
     global date1_entry
     global date2_entry
@@ -246,7 +218,6 @@ def date1():
     next2_button = Button(frame, text='Next', font='Arial 11', bg='black', fg='white', command=next2)
     back2_button = Button(frame, text='Back', font='Arial 11', bg='black', fg='white', command=back2)
 
-    #________________________________
     text1_label.pack()
     year_label.pack()
     date1_entry.pack()
@@ -256,18 +227,14 @@ def date1():
     date3_entry.pack()
     next2_button.pack(padx=10, pady=8)
     back2_button.pack(padx=10, pady=8)
-
-
 def pay():
     frame = Frame(root, bg='white')
     frame.place(relwidth=1, relheight=1)
     pay1_label = Label(frame, text='Payment', font='Arial 15 bold', bg='white', fg='black', )
     ncard_label = Label(frame, text='number payment card', font='Arial 11', bg='white', fg='gray')
     ncard_entry = Entry(frame)
-
     cvccard_label = Label(frame, text='CVC', font='Arial 11', bg='white', fg='gray')
     cvccard_entry = Entry(frame)
-
     next2_button = Button(frame, text='Next', font='Arial 11', bg='black', fg='white', command=next3)
     back2_button = Button(frame, text='Back', font='Arial 11', bg='black', fg='white', command=back3)
     #_____________________________
@@ -278,10 +245,7 @@ def pay():
     cvccard_entry.pack()
     next2_button.pack(padx=10, pady=8)
     back2_button.pack(padx=10, pady=8)
-    sql.execute(f"INSERT INTO tickets VALUES(?, ?, ?)", (user_login2, date, to))
-    db.commit()
     mainloop()
-
 def admin():
     frame = Frame(root, bg='white')
     frame.place(relwidth=1, relheight=1)
@@ -292,10 +256,6 @@ def admin():
     admin_label.pack()
     tickets_btn.pack(padx=10, pady=8)
     users_btn.pack(padx=10, pady=8)
-
-#def back10():
-#   admin()
-
 def tickets():
     frame = Frame(root, bg='white')
     frame.place(relwidth=1, relheight=1)
@@ -306,8 +266,6 @@ def tickets():
         #__________
         label4.pack()
         label5.pack()
-
-
 def users():
     frame = Frame(root, bg='white')
     frame.place(relwidth=1, relheight=1)
@@ -318,6 +276,9 @@ def users():
         # __________
         label2.pack()
         label3.pack()
+def plus():
+    sql.execute(f"INSERT INTO tickets VALUES(?, ?, ?, ?)", (None, user_login2, date, to))
+    db.commit()
 
 
 login_window()
